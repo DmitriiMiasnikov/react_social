546b6567a538cb1d5324a49e8c9c6143ac752f80
DmitriiMiasnikov <51396910+DmitriiMiasnikov@users.noreply.github.com>
2020-08-12 02:59:53 +0300

    Updates

---
+++ precache-manifest.d96aa743cc4cce7c41585a2fb1e3ba7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cc8d0d6116b81dc7ab76f386bf71098",
+    "url": "/react_social/index.html"
+  },
+  {
+    "revision": "3c94ee8367103ecdee38",
+    "url": "/react_social/static/css/main.c18d8239.chunk.css"
+  },
+  {
+    "revision": "db5809476839951741ee",
+    "url": "/react_social/static/js/2.834a133d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react_social/static/js/2.834a133d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c94ee8367103ecdee38",
+    "url": "/react_social/static/js/main.fd499fe9.chunk.js"
+  },
+  {
+    "revision": "cf38743134e4b4d34b88",
+    "url": "/react_social/static/js/runtime-main.2941ba63.js"
+  },
+  {
+    "revision": "848cadae4ecb93f04725abe22f6fe5dd",
+    "url": "/react_social/static/media/loading.848cadae.svg"
+  }
+]);
