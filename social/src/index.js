bb429a48601164e36aecaa77fdd067dc675bba0e
DmitriiMiasnikov <51396910+DmitriiMiasnikov@users.noreply.github.com>
2020-08-17 22:52:35 +0300

    fix index.js

--- social/src/index.js
+++ social/src/index.js
@@ -6,19 +6,11 @@ import App from './App';
 import store from './state/reduxStore';
 import { Provider } from 'react-redux';
 
-let reRender = () => {
-  ReactDOM.render(
-      <Provider store = {store}>
-      <App store = {store}/>
-      </Provider>,
-    document.getElementById('root')
-  );
-}
-reRender(store.getState());
-
-store.subscribe(() => {
-  let state = store.getState();
-  reRender(state);
-});
+ReactDOM.render(
+  <Provider store={store}>
+    <App store={store} />
+  </Provider>,
+  document.getElementById('root')
+);
 
 serviceWorker.unregister();
