f0026bb909a59d0ed826b2ca247d52e5ff78175f
DmitriiMiasnikov <51396910+DmitriiMiasnikov@users.noreply.github.com>
2020-08-20 07:46:59 +0300

    fix

--- social/src/project/Profile/myPosts/MyPosts.js
+++ social/src/project/Profile/myPosts/MyPosts.js
@@ -2,25 +2,28 @@ import React from 'react';
 import styles from './MyPosts.module.scss';
 import Post from './Post/Post';
 
-const MyPosts = (props) => {
-  const postsElements = props.postsData.map((el, i) => {
-    return <Post message={el.message} likesCount={el.likesCount} id={el.id} textPic='pic' key={i} />
-  }),
-    newPostElement = React.createRef();
-  return (
-    <div className={styles.postBlock}>
-      <div className={styles.title}>
-        My posts
+class MyPosts extends React.Component {
+
+  render() {
+    const postsElements = this.props.postsData.map((el, i) => {
+      return <Post message={el.message} likesCount={el.likesCount} id={el.id} textPic='pic' key={i} />
+    }),
+      newPostElement = React.createRef();
+    return (
+      <div className={styles.postBlock}>
+        <div className={styles.title}>
+          My posts
+          </div>
+        <div className={styles.inputBlock}>
+          <textarea ref={newPostElement} value={this.props.newPostText}
+            onChange={() => { this.props.updateNewPost(newPostElement.current.value) }}></textarea>
+          <button onClick={() => this.props.addPost()}>send message</button>
         </div>
-      <div className={styles.inputBlock}>
-        <textarea ref={newPostElement} value={props.newPostText} 
-          onChange={() => {props.updateNewPost(newPostElement.current.value)}}></textarea>
-        <button onClick={() => props.addPost()}>send message</button>
+        <div className={styles.posts}>
+          {postsElements}
+        </div>
       </div>
-      <div className={styles.posts}>
-        {postsElements}
-      </div>
-    </div>
-  );
+    );
+  }
 }
 export default MyPosts;
